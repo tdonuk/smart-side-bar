fb2601eba865962be27c4a26f8dd79da53070adc
taha <rtdonuk@gmail.com>
2023-09-09 23:54:20 +0300

    update

--- js/main.js
+++ js/main.js
@@ -1,26 +1,34 @@
-const GOOGLE_ORIGIN = 'https://www.google.com';
-
 // Allows users to open the side panel by clicking on the action toolbar icon
 chrome.sidePanel
     .setPanelBehavior({openPanelOnActionClick: true})
     .catch((error) => console.error(error));
 
 
-function selectTab(tabHolder, tabId) {
-    console.info("selecting " + tabId + " of " + tabHolder)
-    const holder = document.getElementById(tabHolder);
+function selectTab(tabId) {
+    const holder = document.getElementById("frameHolder");
 
     const tabs = holder.getElementsByClassName("tab");
 
     for (let tab of tabs) {
-        if (tab.id === tabId) {
-            tab.style.display = "block";
-        } else tab.style.display = "none";
+        tab.classList.remove("selected-frame");
     }
+
+    document.getElementById(tabId).classList.add("selected-frame");
+}
+
+function selectTabButton(buttonId) {
+    const holder = document.getElementById("header");
+
+    const buttons = holder.getElementsByClassName("item-box");
+
+    for(let button of buttons) {
+        button.classList.remove("selected");
+    }
+
+    document.getElementById(buttonId).classList.add("selected");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-    console.log("loading..");
     initFrameListeners();
 });
 
@@ -50,35 +58,18 @@ function initFrameListeners() {
             button: document.getElementById("telegramTabTrigger"),
             frame: document.getElementById("telegramFrame")
         },
+        {
+            button: document.getElementById("twitterTabTrigger"),
+            frame: document.getElementById("twitterFrame")
+        },
     ];
 
     for (let buttonConfig of buttonConfigs) {
         buttonConfig.button.onclick = () => {
-            const selectedFrame = getSelectedTab();
-            console.log("selected frame: " + selectedFrame||selectedFrame.id);
-            if(selectedFrame && buttonConfig.frame.id === selectedFrame.id) {
-                console.log("reloading " + buttonConfig.frame.id);
-                selectedFrame.contentWindow.location.reload();
-                return;
-            }
-
-            selectTab("frameHolder", buttonConfig.frame.id);
-
-            buttonConfig.button.classList.add("selected");
-            buttonConfig.frame.classList.add("selected-frame");
-
-            buttonConfigs.forEach(b => {
-                b.button.classList.remove("selected");
-                b.frame.classList.remove("selected-frame");
-            });
+            selectTab(buttonConfig.frame.id);
+            selectTabButton(buttonConfig.button.id);
         }
     }
 
-    buttonConfigs[0].button.onclick(null);
-}
-
-function getSelectedTab() {
-    const tabs = document.getElementById("frameHolder").getElementsByClassName("selected-frame");
-    console.log(JSON.stringify(tabs))
-    return tabs.item(0);
+    buttonConfigs[0].button.onclick();
 }
